9fb894e65e20b2261b549384e250007d5569c5ce
Msukoo <rnalsttnn@gmail.com>
2022-03-06 22:57:42 +0900

    Address 생성자(all, protected default) 추가

--- src/main/java/jpahook/jpashop/domain/entity/Address.java
+++ src/main/java/jpahook/jpashop/domain/entity/Address.java
@@ -1,11 +1,13 @@
 package jpahook.jpashop.domain.entity;
 
+import lombok.AllArgsConstructor;
 import lombok.Getter;
 
 import javax.persistence.Embeddable;
 
 @Embeddable
 @Getter
+@AllArgsConstructor
 public class Address {
 
     private String city;
@@ -13,4 +15,9 @@ public class Address {
     private String street;
 
     private String zipCode;
+
+    protected Address(){
+        // jpa 스팩상 만듬.
+        // 상속할 일이 잘 없으므로 protected로 설정
+    }
 }
