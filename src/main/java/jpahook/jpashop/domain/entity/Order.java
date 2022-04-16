7e7df15f3de252a5bb7e9affefb9941f84a16817
Msukoo <rnalsttnn@gmail.com>
2022-04-16 17:16:13 +0900

    연관관게 set 메서드 추가

--- src/main/java/jpahook/jpashop/domain/entity/Order.java
+++ src/main/java/jpahook/jpashop/domain/entity/Order.java
@@ -23,10 +23,10 @@ public class Order {
     @JoinColumn(name = "member_id")
     private Member member;
 
-    @OneToMany(mappedBy = "order", fetch = FetchType.LAZY)
+    @OneToMany(mappedBy = "order", fetch = FetchType.LAZY, cascade = CascadeType.ALL)
     private List<OrderItem> orderItems = new ArrayList<>();
 
-    @OneToOne(fetch = FetchType.LAZY)
+    @OneToOne(fetch = FetchType.LAZY, cascade = CascadeType.ALL)
     @JoinColumn(name = "delivery_id")
     private Delivery delivery;
 
@@ -34,6 +34,22 @@ public class Order {
 
     @Enumerated(EnumType.STRING)
     private OrderStatus status; // 주문상태 [ORDER, CANCEL]
+
+    //== 연관관계 메서드 ==> 서비스 로직단에서 코드를 줄일 수 있고 하나로 묶어서 두 entity관계 간에 setting을 놓치는 경우를 없//
+    public void setMember(Member member){
+        this.member = member;
+        member.getOrders().add(this);
+    }
+
+    public void addOrderItem(OrderItem orderItem){
+        orderItems.add(orderItem);
+        orderItem.setOrder(this);
+    }
+
+    public void setDelivery(Delivery delivery){
+        this.delivery = delivery;
+        delivery.setOrder(this);
+    }
 
 
 }
